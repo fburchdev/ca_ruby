91076dc245bdeb118ab9f32aadb1c5b195ac1c26
Frances Burch <fburchdev@gmail.com>
2014-06-14 22:37:52 -0400

    Delete from hash

---
+++ ca_movienight_9_10.rb
@@ -0,0 +1,47 @@
+movies = { BlackSwan: 4
+    }
+    
+puts "Please enter either add, update, display, or delete: "
+choice = gets.chomp
+
+case choice
+when "add"
+   puts "Please enter a title: "
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+        puts "Please enter a rating of 1 - 4: "
+        rating = gets.chomp
+        movies[title.intern] = rating.to_i
+        puts "#{title} has been added with a rating of #{rating}."
+    else
+        puts "#{title} already exists!"
+    end
+when "update"
+    puts "Please enter a title to update: "
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+        puts "#{title} does not exist. Please add movie before updating."
+    else
+        puts "Please enter a new 1-4 rating for #{title}: "
+        rating = gets.chomp
+        movies[title.to_sym] = rating.to_i
+        puts "#{title} updated with new rating of #{rating}"
+    end
+when "display"
+    movies.each do |movie, rating|
+        puts "#{movie}: #{rating}"
+    end
+when "delete"
+    puts "Please enter the movie title to delete: "
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+        puts "#{title} cannot be deleted. Please add before deleting."
+    else
+        movies[title.to_sym].delete
+        puts "#{title} deleted"
+    end
+else
+    puts "Error!"
+end
+
+
