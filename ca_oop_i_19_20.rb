3c2f904bb8898d1f5d0734b730a901f11347b9b7
Frances Burch <fburchdev@gmail.com>
2015-09-04 15:06:01 -0400

    Object-Oriented Programming I Exercises 19-20

---
+++ ca_oop_i_19_20.rb
@@ -0,0 +1,15 @@
+class Message
+    @@messages_sent = 0
+    def initialize(from, to)
+        @from = from
+        @to = to
+        @@messages_sent += 1
+    end
+end
+my_message = Message.new("Customer", "Company")
+class Email < Message
+    def initialize(subject)
+        @subject = subject
+    end
+end
+
