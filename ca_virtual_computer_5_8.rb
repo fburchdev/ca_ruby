aba96bdcdf006bf97b00df16aa9d12d9cbc24829
Frances Burch <fburchdev@gmail.com>
2015-09-04 16:02:39 -0400

    Virtual Computer Exercises 5-8

---
+++ ca_virtual_computer_5_8.rb
@@ -0,0 +1,14 @@
+class Computer
+    @@users = {}
+    def initialize(username, password)
+        @username = username
+        @password = password
+        @files = {}
+        @@users[username] = password
+    end
+    def create(filename)
+        time = Time.now
+        @files[filename] = time
+        puts "A new file was created: #{filename}, user: #{@username}, time: #{time}."
+    end
+end
---
+++ ca_virtual_computer_7_8.rb
@@ -0,0 +1,18 @@
+class Computer
+    @@users = {}
+    def initialize(username, password)
+        @username = username
+        @password = password
+        @files = {}
+        @@users[username] = password
+    end
+    def create(filename)
+        time = Time.now
+        @files[filename] = time
+        puts "A new file was created: #{filename}, user: #{@username}, time: #{time}."
+    end
+    def Computer.get_users
+        return @@users
+    end
+end
+my_computer = Computer.new("Frances", 1234)
